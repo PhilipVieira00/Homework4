c73b4d94cee91c39d3a82ba7bb11644e8a32b929
Philip Vieira <philipvieira00@gmail.com>
2019-10-04 00:35:44 -0400

    Finally fixed the game

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,33 +1,47 @@
+var computerChoice = "";
+var chances = 5;
+var win = 0;
+var loss = 0;
+
+
+function generateArray() {
+  var compArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var compGen = Math.floor(Math.random() * compArray.length);
+  computerChoice = compArray[compGen];
+}
+
 function gameplay() {
-
-      let win = 0;
-      let wins = document.getElementById("wins");
-
-      let loss = 0;
-      let losses = document.getElementById("losses");
-
-      let userVar = document.getElementById("userChoice");
+  var losses = document.getElementById("losses");
+  var wins = document.getElementById("wins");
+  var userVar = document.getElementById("userChoice");
+      generateArray();
       document.onkeyup = function(event) {
       userVar.textContent = event.key;
-      let userChoice = event.key;
+      var userChoice = event.key;
 
-      let compArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-      let computerChoice = document.getElementById("computerChoice");
-      let compGen = Math.floor(Math.random() * compArray.length);
-      computerChoice.textContent = compArray[compGen];
+      console.log(computerChoice);
 
-      console.log(compArray[compGen]);
-
-      if ((userChoice===compArray[compGen])) {
+      if ((userChoice===computerChoice)) {
         win++;
         wins.textContent = win;
+        chances = 5;
+        generateArray();
+        gameplay();
       }
 
       else {
+        if (chances === 0) {
             loss++;
             losses.textContent = loss;
-          
+            chances=5;
+            generateArray();
+            gameplay();
+        }
+        else {
+          chances--;
+        }
       }
       };
     }
+    
 
